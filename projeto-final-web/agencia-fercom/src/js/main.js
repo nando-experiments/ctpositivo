f5e3b28da26d5a6235592b1e4de6c7a4121214e1
Fernando Moreira <nandomoreira.me@gmail.com>
2015-09-24 21:14:42 -0300

    wow.js and animate.css

---
+++ projeto-final-web/agencia-fercom/src/js/main.js
@@ -0,0 +1,14 @@
+
+//= include ../bower/jquery/dist/jquery.min.js
+
+//= include ../bower/wow/dist/wow.min.js
+
+wow = new WOW({
+  boxClass     : 'wow',
+  animateClass : 'animated',
+  offset       : 0,
+  mobile       : true,
+  live         : true
+});
+
+wow.init();
